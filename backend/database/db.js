56730b0483271a4c30cda5dd327b73077fa14b5e
yuvraj <162401167+yuvrajsingh101280@users.noreply.github.com>
2025-04-03 03:40:42 +0000

    dsd

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,12 @@ import "dotenv/config.js"
 const connectToMongoDb = async () => {
     const URL = process.env.DATABASE
     try {
-        await connect(URL)
+        await connect(URL,{useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+            autoIndex: false, // Disable automatic index creation
+            }
+            )
         console.log("database connected successfully")
     } catch (error) {
         console.log("DAtabase not connected sorry", error)
